d1117f3980b8cfe153cf95ee79742b21cdb08343
Smrity Baral <baralsmrity8@gmail.com>
2024-01-24 17:39:52 +0530

    class based and function based detailed view

--- src/products/urls.py
+++ src/products/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductListView.as_view(), name='Products'),
+    path('products/<int:pk>/', views.ProductDetailView.as_view(),
+         name='ProductDetail'),
     path('products/fnc/', views.product_list_view, name='product_list'),
+    path('products/fnc/<int:pk>/', views.product_detail_view, name='product_detail'),
     # Other app1 URLs
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,9 @@
 from typing import Any
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 
 
 from .models import Product
@@ -13,13 +16,25 @@ from .models import Product
 class ProductListView(ListView):
     model = Product
     querySet = Product.objects.all()  # get all the products from model Product
-    template_name = 'products/product_list.html'
+    template_name = 'products/list.html'
 
     # def get_context_data(self, *args, **kwargs):
     #     context = super(ProductListView, self).get_context_data(
     #         *args, **kwargs)
     #     print(context)
     #     return context
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    querySet = Product.objects.all()  # get all the products from model Product
+    template_name = 'products/detail.html'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset=queryset)
+        if obj is None:
+            raise Http404("No product found matching the query")
+        return obj
 
 
 # Function Based
@@ -29,4 +44,20 @@ def product_list_view(request):
     context = {
         "object_list": querySet
     }
-    return render(request, "products/product_list.html", context)
+    return render(request, "products/list.html", context)
+
+
+# def product_detail_view(request, pk=None, *args, **kwargs):
+#     # instance = Product.objects.get(pk=pk)
+#     instance = get_object_or_404(Product, pk=pk)
+#     context = {
+#         "object": instance
+#     }
+#     return render(request, "products/detail.html", context)
+def product_detail_view(request, pk=None, *args, **kwargs):
+    try:
+        instance = get_object_or_404(Product, pk=pk)
+        context = {"object": instance}
+        return render(request, "products/detail.html", context)
+    except Http404:
+        return render(request, "products/404.html", {"error_message": "No product found matching the query"}, status=404)
