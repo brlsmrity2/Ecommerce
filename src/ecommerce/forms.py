6c47f987aedc58eb4adf7127f0923431c15387e4
Smrity Baral <baralsmrity8@gmail.com>
2024-01-24 12:27:08 +0530

    user login and registration

--- src/ecommerce/forms.py
+++ src/ecommerce/forms.py
@@ -3,61 +3,65 @@ from django import forms
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+
 class ContactForm(forms.Form):
     fullname = forms.CharField(
-        widget=forms.TextInput(attrs={'class':'form-control','placeholder': 'Your Full Name'}),
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Your Full Name'}),
         max_length=100,
         required=True
     )
     email = forms.EmailField(
-        widget=forms.EmailInput(attrs={'class':'form-control','placeholder': 'Your Email'}),
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control', 'placeholder': 'Your Email'}),
         max_length=100,
         required=True
     )
     content = forms.CharField(
-        widget=forms.Textarea(attrs={'class':'form-control','placeholder': 'Your Message'}),
+        widget=forms.Textarea(
+            attrs={'class': 'form-control', 'placeholder': 'Your Message'}),
         required=True
     )
 
 
-#custom validation
+# custom validation
+
 
     def clean_email(self):
         email = self.cleaned_data.get("email")
         if not "gmail.com" in email:
-            raise forms.ValidationError("Email Has to be Gmail")        
+            raise forms.ValidationError("Email Has to be Gmail")
         return email
-    
-    
+
+
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput) 
-    
+    password = forms.CharField(widget=forms.PasswordInput)
 
 
 class RegisterForm(forms.Form):
     username = forms.CharField()
     email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)     
-    password2 = forms.CharField(label = "Confirm password",widget=forms.PasswordInput)   
-    
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Confirm password", widget=forms.PasswordInput)
+
     def clean_username(self):
-        username =self.cleaned_data.get("username")
-        qs = User.objects.filter(username = username)
+        username = self.cleaned_data.get("username")
+        qs = User.objects.filter(username=username)
         if qs.exists():
             raise forms.ValidationError("Username already taken")
-        
+
     def clean_email(self):
-            email =self.cleaned_data.get("email")
-            qs = User.objects.filter(email = email)
-            if qs.exists():
-                raise forms.ValidationError("Email already taken")
-    
+        email = self.cleaned_data.get("email")
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError("Email already taken")
+
     def clean(self):
-        data =self.cleaned_data
-        password =self.cleaned_data.get("password")
+        data = self.cleaned_data
+        password = self.cleaned_data.get("password")
         password2 = self.cleaned_data.get("password2")
         if password2 != password:
             raise forms.ValidationError("Passwords must match")
         return data
-        
--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,37 +1,41 @@
-from django.contrib.auth import authenticate,login,get_user_model
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from .forms import ContactForm,LoginForm, RegisterForm
+from .forms import ContactForm, LoginForm, RegisterForm
+
 
 def home_page(request):
-    context = {"title":"Smrity"}
-    return render(request, "home_page.html",context)
+    context = {"title": "Smrity"}
+    return render(request, "home_page.html", context)
+
 
 def about_page(request):
-    return render(request, "about_page.html",{})
+    return render(request, "about_page.html", {})
+
 
 def contact_page(request):
     contact_form = ContactForm(request.POST or None)
     context = {
-        'title':"CONTACT",
-        "content":"Welcome to Contact Page",
-        "form":contact_form
+        'title': "CONTACT",
+        "content": "Welcome to Contact Page",
+        "form": contact_form
     }
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
-    return render(request, "contact/view.html",context) 
+    return render(request, "contact/view.html", context)
+
 
 def login_page(request):
     form = LoginForm(request.POST or None)
-    context={
-        'form':form
+    context = {
+        'form': form
     }
     print("user logged in")
     print(request.user.is_authenticated)
     if form.is_valid():
-        print(form.cleaned_data) 
-        #context['form']=LoginForm()#clears for new form
+        print(form.cleaned_data)
+        # context['form']=LoginForm()#clears for new form
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password")
         user = authenticate(request, username=username, password=password)
@@ -41,19 +45,22 @@ def login_page(request):
             # Redirect to a success page.
         else:
             print("Error")
-    return render(request,'auth/login.html',context)
+    return render(request, 'auth/login.html', context)
 
-User =get_user_model()
+
+User = get_user_model()
+
+
 def register_page(request):
     form = RegisterForm(request.POST or None)
-    context={
-        "form":form
+    context = {
+        "form": form
     }
     if form.is_valid():
         print(form.cleaned_data)
         username = form.cleaned_data.get("username")
         email = form.cleaned_data.get("email")
         password = form.cleaned_data.get("password")
-        User.objects.create_user(username,email,password)
+        User.objects.create_user(username, email, password)
         print(User)
-    return render(request,'auth/register.html',context)
+    return render(request, 'auth/register.html', context)
